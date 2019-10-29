b253a823adeaaf0980f9b2b286c2778662ed033c
angelcapella <44397399+angelcapella@users.noreply.github.com>
2019-10-29 11:10:15 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+1º paso : vagrant up
+2º paso : vagrant ssh
+3º paso : SE DESCARGARA EL APACHA AUTOMATICAMENTE
---
+++ provision.sh
@@ -0,0 +1,5 @@
+apt-get install -y apache2
+
+sudo mkdir/vagrant/html
+sudo mkdir/vagrant/html/web1
+sudo mkdir/vagrant/html/web2
